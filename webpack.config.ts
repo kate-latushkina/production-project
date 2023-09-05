aad768bfc70d78bffb63709a4182286401952b80
ekaterina.latushkina <katia_sali@mail.ru>
2023-09-05 18:53:30 +0300

    feat: add webpack-devserver

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,24 @@
 import path from 'path';
 import webpack from 'webpack';
-import HTMLWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
+import { BuildEnv, BuildPath } from './config/build/types/config';
 
-const config: webpack.Configuration = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        filename: "[name].[contenthash].js",
-        path: path.resolve(__dirname, 'build'),
-        clean: true
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html')
-        }),
-        new webpack.ProgressPlugin(),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            }
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-}
+export default (env: BuildEnv) => {
+    const paths: BuildPath = {
+        entry: path.resolve(__dirname, 'src', 'index.ts'),
+        build: path.resolve(__dirname, 'build'),
+        html: path.resolve(__dirname, 'public', 'index.html')
+    };
+    
+    const PORT = env.port || 5000;
+    const mode = env.mode || 'development';
+    const isDev = mode === 'development';
+    const config: webpack.Configuration = buildWebpackConfig({
+        mode,
+        paths,
+        isDev,
+        port: PORT,
+    });
 
-export default config;
+    return config;
+};
