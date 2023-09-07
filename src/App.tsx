1de293fd7f257949bd6f35a961a975cb13d56f86
ekaterina.latushkina <katia_sali@mail.ru>
2023-09-07 21:41:19 +0300

    feat: add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
-import { Counter } from './components/Counter';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import "./index.scss"
+import { Link } from 'react-router-dom';
+import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
+import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
+import { Suspense } from 'react';
 
 const App = () => {
     return (
         <div className='app'>
-            fskjfbgvsdbfvksjd
-            <Counter/>
+            <BrowserRouter>
+            <Link to={'/'}>Main</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageLazy/>}/>
+                    <Route path={'/'} element={<MainPageLazy/>}/>
+                </Routes>
+            </Suspense>    
+            </BrowserRouter>
         </div>
     );
 };
