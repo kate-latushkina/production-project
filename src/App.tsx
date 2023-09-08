592eb1eba231896cc75ae64262c7928d579dca33
ekaterina.latushkina <katia_sali@mail.ru>
2023-09-08 20:17:34 +0300

    feat: add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import "./index.scss"
+import { Route, Routes } from 'react-router-dom';
+import "./styles/index.scss"
 import { Link } from 'react-router-dom';
 import { AboutPageLazy } from './pages/AboutPage/AboutPage.lazy';
 import { MainPageLazy } from './pages/MainPage/MainPage.lazy';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme}  = useTheme();
     return (
-        <div className='app'>
-            <BrowserRouter>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Change theme</button>
             <Link to={'/'}>Main</Link>
             <Link to={'/about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
@@ -17,7 +19,6 @@ const App = () => {
                     <Route path={'/'} element={<MainPageLazy/>}/>
                 </Routes>
             </Suspense>    
-            </BrowserRouter>
         </div>
     );
 };
