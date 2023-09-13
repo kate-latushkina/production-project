def684a7c3285af1426cbc802563697c5ae922b4
ekaterina.latushkina <katia_sali@mail.ru>
2023-09-13 14:52:46 +0300

    feat: add babel and extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -38,9 +38,30 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[] {
             loader: 'file-loader',
           },
         ],
-      };
+    };
+
+    const babelLoader = {
+      test: /\.(js|jsx|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: "babel-loader",
+        options: {
+          presets: ['@babel/preset-env'],
+          "plugins": [
+            [
+              "i18next-extract",
+              {
+                locales: ['en', 'ru'],
+                keyAsDefaultValue: true
+              }
+            ],
+          ]
+        }
+      }
+    };
     
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
