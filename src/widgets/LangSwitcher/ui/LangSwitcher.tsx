8327cfce143c3dae96b33ac28391eda119cdcd5c
ekaterina.latushkina <katia_sali@mail.ru>
2023-10-03 22:46:36 +0300

    fix: change sidebar and add icons

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,13 +1,14 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./LangSwitcher.module.scss";
 import { useTranslation } from "react-i18next";
-import { Button, ThemeButton } from "shared/ui/Button/Button";
+import { Button, ButtonTheme } from "shared/ui/Button/Button";
 
 interface LangSwitcherProps {
   className?: string
+  short?: boolean
 }
 
-export const LangSwitcher = ({ className = "" }: LangSwitcherProps): JSX.Element => {
+export const LangSwitcher = ({ className = "", short = false }: LangSwitcherProps): JSX.Element => {
   const { t, i18n } = useTranslation();
   const onToggleLang = (): void => {
     void i18n.changeLanguage(i18n.language === "en" ? "ru" : "en");
@@ -16,10 +17,10 @@ export const LangSwitcher = ({ className = "" }: LangSwitcherProps): JSX.Element
   return (
       <Button
           onClick={onToggleLang}
-          theme={ThemeButton.CLEAR}
+          theme={ButtonTheme.CLEAR}
           className={classNames(cls.LangSwitcher, {}, [className])}
         >
-          {t("Translate")}
+          {t(short ? "EN/RU" : "Translate")}
       </Button>
   );
 };
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,13 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
-import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { useTranslation } from "react-i18next";
 
 interface NavbarProps {
   className?: string
 }
 
 export const Navbar = ({ className }: NavbarProps): JSX.Element => {
-  const { t } = useTranslation();
-
   return (
       <div className={classNames(cls.Navbar, {}, [cls.links])}>
-          <AppLink to={"/"} className={cls.mainLink} theme={AppLinkTheme.SECONDARY}>{t("Main")}</AppLink>
-          <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>{t("About")}</AppLink>
       </div>
   );
 };
